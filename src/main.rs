42a6ce76f4182798b5fa2c473c7aa5bc888ee1ce
Preetham <preetham.guduri1@gmail.com>
2026-08-11 14:06:44 -0500

    Updated Config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use defmt_rtt as _;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Pin, Speed};
+use embassy_time::Timer;
+use panic_probe as _;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let mut config = embassy_stm32::Config::default();
+    config.enable_debug_during_sleep = true; // <-- CRITICAL FIX
+    let p = embassy_stm32::init(config);
+    let mut led = Output::new(p.PA5, Level::High, Speed::Low);
+
+    loop {
+        led.toggle();
+        Timer::after_millis(500).await;
+    }
 }
